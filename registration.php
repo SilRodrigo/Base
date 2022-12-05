2f300312266de054e260996d8059d1d0cd4d4380
silRodrigo <rodrigo.sil91@gmail.com>
2022-12-05 19:55:44 -0300

    first commit

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @author Rodrigo Silva
+ * @copyright Copyright (c) 2022 Rodrigo Silva (https://github.com/SilRodrigo)
+ * @package Rsilva_Base
+ */
+
+ use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Rsilva_Base',
+    __DIR__
+);
